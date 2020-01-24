7afedd941e2d0030b7b28e12e92d4eb03baa320e
Isaiah St. John <hello@isaiah.st>
2020-01-23 21:32:16 -0500

    initial commit

---
+++ autorunner.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+/*
+ * Accept a file name to watch
+ *
+ * Check last-modified timestamp for changes in loop
+ *
+ * On change, execute file
+ *
+ */
+const fs = require('fs');
+const {exec} = require('child_process');
+
+const watchFilePath = process.argv[2] || 'main.js';
+
+let lastMod;
+setInterval(() => {
+  lastModified(watchFilePath, (newLastMod) => {
+    if(newLastMod !== lastMod){
+      lastMod = newLastMod;
+      run(watchFilePath);
+    }  
+  });
+}, 50);
+
+function lastModified(filePath, callback){
+  const interval = setInterval(() => {
+    fs.stat(filePath, (err, stats) => {
+      if(stats){
+        clearInterval(interval);
+        callback(stats.mtimeMs);
+      }
+    });
+  }, 10);
+}
+
+function run(filePath){
+  exec(`node ${filePath}`, (err, stdout, stderr) => {
+    console.log([err, stdout, stderr]
+      .filter((cur) => Boolean(cur))
+      .join('\n\n')
+      .trim()
+    );
+  });
+}
+
+
+
